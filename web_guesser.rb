80c59bb3dab67f0935eb9beab93a0b76cebad35a
David Knott <david1k1nott@gmail.com>
2016-11-19 21:26:16 -0700

    adds in background color changes

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,20 +6,25 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"]
   message = check(guess)
-  erb :index, :locals => {:message => message}
+  erb :index, :locals => {:message => message, :color => @color}
 end
 
 def check(guess)
   difference = guess.to_i - SECRET_NUMBER
   if difference < -5
+    @color = "red"
     "Way too low!"
   elsif difference < 0
+    @color = "salmon"
     "Too low!"
   elsif  difference > 5
+    @color = "red"
     "Way too high!"
   elsif  difference > 0
+    @color = "salmon"
     "Too high!"
   else
+    @color = "lime"
     "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
   end
 end
